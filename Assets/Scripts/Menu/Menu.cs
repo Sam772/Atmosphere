1acf3ea06e92e8f53d0edacd82c5e562ae5f3bfd
Random <78389553+Sam772@users.noreply.github.com>
2024-02-16 21:56:42 +0000

    StartScreen2

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour {
     [SerializeField] private StartScreen _startScreen;
+    [SerializeField] private FileSelectScreen _fileSelectScreen;
     private MenuScreen _currentScreen;
 
     private void Awake() {
@@ -12,14 +13,17 @@ public class Menu : MonoBehaviour {
 
     private void Start() {
         _startScreen.Setup(this);
+        _fileSelectScreen.Setup(this);
     }
 
     public void ShowStartScreen() => ShowScreen(_startScreen);
+    public void ShowFileSelectScreen() => ShowScreen(_fileSelectScreen); 
 
     private void ShowScreen(MenuScreen screen) {
         if (_currentScreen == screen) return;
 
         if (screen != _startScreen) _startScreen.Hide();
+        if (screen != _fileSelectScreen) _fileSelectScreen.Hide();
 
         screen.Show();
         _currentScreen = screen;
