6e166f763ef85594f9fb66ea6bf321e3aace3aa1
Random <78389553+Sam772@users.noreply.github.com>
2024-04-24 03:52:24 +0100

    Level 2 design

--- Assets/Scripts/Game/Characters/Player/PlayerController.cs
+++ Assets/Scripts/Game/Characters/Player/PlayerController.cs
@@ -9,6 +9,9 @@ public class PlayerController : MonoBehaviour, ICharacter {
     [SerializeField] private LayerMask _groundLayer;
     [SerializeField] private Rigidbody _rigidBody;
     [SerializeField] private Transform _cameraTransform;
+    [SerializeField] private Renderer _renderer;
+    [SerializeField] private Material _playerIdleMat;
+    [SerializeField] private Material _playerRunMat;
     private bool _isGrounded;
 
     public int Diamonds => SaveData.Current.Diamonds;
@@ -21,24 +24,30 @@ public class PlayerController : MonoBehaviour, ICharacter {
         _rigidBody.freezeRotation = true;
     }
 
-    void Update() {
+        void Update() {
         _isGrounded = Physics.CheckSphere(_groundCheck.position, 0.1f, _groundLayer);
+        HandleMovement();
+        HandleJumping();
+        RotateToGround();
+        ApplyGravity();
+    }
 
+    void HandleMovement() {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-
         Vector3 moveDirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
-
         moveDirection = Quaternion.AngleAxis(_cameraTransform.rotation.eulerAngles.y, Vector3.up) * moveDirection;
-
         Vector3 moveVelocity = transform.TransformDirection(moveDirection) * _moveSpeed;
-
         _rigidBody.velocity = new Vector3(moveVelocity.x, _rigidBody.velocity.y, moveVelocity.z);
+    }
 
+    void HandleJumping() {
         if (_isGrounded && Input.GetButtonDown("Jump")) {
             _rigidBody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
         }
+    }
 
+    void RotateToGround() {
         if (_isGrounded) {
             RaycastHit hit;
             if (Physics.Raycast(transform.position, -Vector3.up, out hit, 1.0f, _groundLayer)) {
@@ -46,7 +55,9 @@ public class PlayerController : MonoBehaviour, ICharacter {
                 transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 10f * Time.deltaTime);
             }
         }
+    }
 
+    void ApplyGravity() {
         if (_isGrounded) {
             _rigidBody.AddForce(Physics.gravity * _rigidBody.mass);
         }
